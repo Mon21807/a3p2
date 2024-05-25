7b1355191b4c346982c3a7001ddf91b8a7354526
Sahil18799 <sahil18799@gmail.com>
2024-05-25 11:09:11 +0530

    first commit

---
+++ src/components/ChangeColor2.jsx
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+
+function ChangeColor2() {
+  const divRef = useRef(null);
+
+  const handleChangeColor = () => {
+    divRef.current.style.backgroundColor = "blue";
+  };
+
+  return (
+    <div>
+      <div
+        ref={divRef}
+        style={{ width: "100px", height: "100px", backgroundColor: "red" }}
+      >
+        Color Box
+      </div>
+      <button onClick={handleChangeColor}>Change Color</button>
+    </div>
+  );
+}
+
+export default ChangeColor2;
